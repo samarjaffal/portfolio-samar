aeaca889c2d5277f9af62ce7ea8a224a2e3b61d6
Samar Jaffal <samarjaffalh@gmail.com>
2022-04-30 15:02:15 -0400

    improved style

---
+++ src/projects.mjs
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    name: 'Wuptick',
+    color: 'text-primary',
+    description: 'Wuptick is a cool web application to manage and control teams, projects and tasks in one place.',
+    techs: ['javascript', 'react', 'styled-components', 'graphql', 'nodejs'],
+    image: 'images/wuptick.png'
+  },
+  {
+    name: 'Wunderhod',
+    color: 'text-secondary',
+    description: 'An application developed for testing purposes with React Native and Expo to have access to all service providers from Margarita, Nueva Esparta. Venezuela.',
+    techs: ['javascript', 'react', 'firebase'],
+    image: 'images/wunderhod.png'
+  },
+  {
+    name: 'HelloChain',
+    color: 'text-tertiary',
+    description: 'A web application for testing purposes when a user can sign in using Metamask and leave a "hello message" through te blockchain.',
+    techs: ['javascript', 'react', 'tailwindcss', 'solidity', 'hardhat'],
+    image: 'images/hellochain.png'
+  }
+]
